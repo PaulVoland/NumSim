42cdc87f290b4552b7f84b435a0086b870476d51
MarcelMe <st142227@stud.uni-stuttgart.de>
2018-11-16 14:02:20 +0100

    constructor gatherSum gatherMax gatherMin are implement

--- src/communicator.cpp
+++ src/communicator.cpp
@@ -1,62 +1,54 @@
-#include "compute.hpp"
-#include "geometry.hpp"
-#include "parameter.hpp"
+#include "communicator.hpp"
 #include "grid.hpp"
-#include "solver.hpp"
-#include "iterator.hpp"
-
+#include "typedef.hpp"
 #include <iostream>
 #include <cmath>
+#include <unistd.h>
+#include <mpi.h>
+#include <stdio.h>
 
 using namespace std;
 
 //------------------------------------------------------------------------------
-class Communicator {
-public:
   /** Communicator constructor; initializes MPI Environment
    *
    * \param [in] argc Number of arguments program was started with
    * \param [in] argv Arguments passed to the program on start
    */
   Communicator::Communicator(int *argc, char ***argv){
-      MPI_Init(NULL, NULL);
-      MPI_Comm_size(MPI_COMM_WORLD, _size);
-      MPI_Comm_rank(MPI_COMM_WORLD, _rank);
+
+      MPI_Init(argc,argv);
+      MPI_Comm_size(MPI_COMM_WORLD, &_size);
+      MPI_Comm_rank(MPI_COMM_WORLD, &_rank);
   }
   /** Communicator destructor; finalizes MPI Environment
    */
-  ~Communicator(){
-    delete[] _rank;
-    delete[] _size;
-    delete[] _tidx;
-    delete[] _evenodd;
-    delete[] _tdim; 
+  Communicator::~Communicator(){
+
   }
 
   /** Returns the position of the current process with respect to the
    *  fields lower left corner
    */
-  const multi_index_t &ThreadIdx() const{ return _tidx; }
+  const multi_index_t& Communicator::ThreadIdx() const {return _tidx;}
 
   /** Returns the way the domain is partitioned among all processes
    */
-  const multi_index_t &ThreadDim() const{ return _tdim; }
+  const multi_index_t& Communicator::ThreadDim() const {return _tdim;}
 
   /** Returns whether this process is a red or a black field
    */
-  const bool &EvenOdd() const { return _evenodd; }
+  const bool& Communicator::EvenOdd() const {return _evenodd;}
 
   /** Gets the sum of all values and distributes the result among all
    *  processes
    *
    * \param [in] val The data over which the sum is to be calculated
    */
-  real_t gatherSum(const real_t &val) const {
-    if (_rank == 0){
-      
-    }
-  }
-  return max;
+  real_t Communicator::gatherSum(const real_t &val) const {
+  	real_t sum = 0;
+  	MPI_Allreduce(&val,&sum,1,MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
+  	return sum;
   }
 
   /** Finds the minimum of the values and distributes the result among
@@ -64,51 +56,55 @@ public:
    *
    * \param [in] val The data over which to find the minimum
    */
-  real_t gatherMin(const real_t &val) const;
+  real_t Communicator::gatherMin(const real_t &val) const{
+  	real_t min = 0;
+  	MPI_Allreduce(&val,&min,1,MPI_DOUBLE,MPI_MIN,MPI_COMM_WORLD);
+  	return min;
+  }
+
 
   /** Finds the maximum of the values and distributes the result among
    *  all processes
    *
    * \param [in] val The data over which to find the maximum
    */
-  real_t gatherMax(const real_t &val) const;
+  real_t Communicator::gatherMax(const real_t &val) const{
+  	real_t max = 0;
+  	MPI_Allreduce(&val,&max,1,MPI_DOUBLE,MPI_MAX,MPI_COMM_WORLD);
+  	return max;
+  }
 
   /** Synchronizes ghost layer
    *
    * \param [in] grid  The values to sync
    */
-  void copyBoundary(Grid *grid) const;
+  void Communicator::copyBoundary(Grid *grid) const{}
 
   /** Decide whether our left boundary is a domain boundary
    */
-  const bool isLeft() const;
+  const bool Communicator::isLeft() const{ return _evenodd; }
 
   /** Decide whether our right boundary is a domain boundary
    */
-  const bool isRight() const;
+  const bool Communicator::isRight() const{ return _evenodd; }
 
   /** Decide whether our top boundary is a domain boundary
    */
-  const bool isTop() const;
+  const bool Communicator::isTop() const{ return _evenodd; }
 
   /** Decide whether our bottom boundary is a domain boundary
    */
-  const bool isBottom() const;
+  const bool Communicator::isBottom() const{ return _evenodd; }
 
   /** Get MPI rank of current process
    */
-  const int &getRank() const;
+  const int& Communicator::getRank() const{ return _rank; }
 
   /** Get number of MPI processes
    */
-  const int &getSize() const;
+  const int& Communicator::getSize() const{return _rank; }
 
-private:
-  multi_index_t _tidx;
-  multi_index_t _tdim;
-  int _rank;
-  int _size;
-  bool _evenodd;
+
 
   /** Function to sync ghost layer on left boundary:
    *  send values of own left boundary to left neighbor and
@@ -127,27 +123,27 @@ private:
    *
    * \param [in] grid  values whose boundary shall be synced
    */
-  bool copyLeftBoundary(Grid *grid) const;
+  bool Communicator::copyLeftBoundary(Grid *grid) const{ return _evenodd; }
 
   /** Function to sync ghost layer on right boundary
    *  Details analog to left boundary
    *
    * \param [in] grid  values whose boundary shall be synced
    */
-  bool copyRightBoundary(Grid *grid) const;
+  bool Communicator::copyRightBoundary(Grid *grid) const{ return _evenodd; }
 
   /** Function to sync ghost layer on top boundary
    *  Details analog to left boundary
    *
    * \param [in] grid  values whose boundary shall be synced
    */
-  bool copyTopBoundary(Grid *grid) const;
+  bool Communicator::copyTopBoundary(Grid *grid) const{ return _evenodd; }
 
   /** Function to sync ghost layer on bottom boundary
    *  Details analog to left boundary
    *
    * \param [in] grid  values whose boundary shall be synced
    */
-  bool copyBottomBoundary(Grid *grid) const;
-};
+  bool Communicator::copyBottomBoundary(Grid *grid) const{ return _evenodd; }
+
 //------------------------------------------------------------------------------
