0d9c6109b8e4bdc8a521bdce731899be8d3265a4
PaulVoland <p.c.m.voland@gmail.com>
2018-11-19 13:11:47 +0100

    geom->Size() already with halo included => changes in methods required

--- src/iterator.cpp
+++ src/iterator.cpp
@@ -3,59 +3,60 @@
 //------------------------------------------------------------------------------
 /// Constructs a new Iterator depending on a geometry
 // @param geom input geometry to iterate over
-Iterator::Iterator(const Geometry* geom) {
-  _geom = geom;
+Iterator::Iterator(const Geometry* geom) : _geom(geom){
   First();
-  _increm_x = geom->Size()[1] + 2;
-  _increm_y = geom->Size()[0] + 2;
+  _increm_x = geom->Size()[1];
+  _increm_y = geom->Size()[0];
   _num_cell = _increm_x*_increm_y;
 }
-
+//------------------------------------------------------------------------------
 /// Constructs a new Iterator on a geometry with a defined starting value
 // @param geom  input geometry to iterate over
 // @param value starting index for iteration
-Iterator::Iterator(const Geometry* geom, const index_t& value) {
-  _geom = geom;
+Iterator::Iterator(const Geometry* geom, const index_t& value) : _geom(geom) {
   _value = value;
   _valid = true;
-  _increm_x = geom->Size()[1] + 2;
-  _increm_y = geom->Size()[0] + 2;
+  _increm_x = geom->Size()[1];
+  _increm_y = geom->Size()[0];
   _num_cell = _increm_x*_increm_y;
+  if (_value >= _num_cell)
+    _valid = false;
 }
-
+//------------------------------------------------------------------------------
 /// Returns the current position value
 const index_t& Iterator::Value() const {return _value;}
-
+//------------------------------------------------------------------------------
 /// Cast operator to convert Iterators to integers
 Iterator::operator const index_t&() const {return _value;}
-
+//------------------------------------------------------------------------------
 /// Returns the position coordinates
 multi_index_t Iterator::Pos() const {
   multi_index_t pos;
   pos[0] = _value % _increm_y;
-  pos[1] = (_value - pos[0]) / _increm_x;
+  pos[1] = (index_t) (_value / _increm_x);
   return pos;
 }
-
+//------------------------------------------------------------------------------
 /// Sets the iterator to the first element
 void Iterator::First() {
   _value = 0;
   _valid = true;
 }
-
+//------------------------------------------------------------------------------
 /// Goes to the next element of the iterator, disables it if position is end
 void Iterator::Next() {
+  if (!_valid) {return;}
   if (_value < _num_cell - 1)
     _value++;
   else
     _valid = false;
 }
-
+//------------------------------------------------------------------------------
 /// Checks if the iterator still has a valid value
 bool Iterator::Valid() const {
   return _valid;
 }
-
+//------------------------------------------------------------------------------
 /// Returns an Iterator that is located left from this one.
 // If we are at the left boundary, the cell sees itself.
 Iterator Iterator::Left() const {
@@ -64,7 +65,7 @@ Iterator Iterator::Left() const {
   else
     return Iterator(_geom, _value - 1);
 }
-
+//------------------------------------------------------------------------------
 /// Returns an Iterator that is located right from this one.
 // If we are at the right boundary, the cell sees itself.
 Iterator Iterator::Right() const {
@@ -73,7 +74,7 @@ Iterator Iterator::Right() const {
   else
     return Iterator(_geom, _value + 1);
 }
-
+//------------------------------------------------------------------------------
 /// Returns an Iterator that is located above this one.
 // If we are at the upper domain boundary, the cell sees itself.
 Iterator Iterator::Top() const {
@@ -83,7 +84,7 @@ Iterator Iterator::Top() const {
   else
     return Iterator(_geom, _value_top);
 }
-
+//------------------------------------------------------------------------------
 /// Returns an Iterator that is located below this one.
 // If we are at the lower domain boundary, the cell sees itself.
 Iterator Iterator::Down() const {
@@ -93,7 +94,6 @@ Iterator Iterator::Down() const {
   else
     return Iterator(_geom, _value_down);
 }
-
 //------------------------------------------------------------------------------
 /* Iterator for interior cells
 */
@@ -102,15 +102,16 @@ Iterator Iterator::Down() const {
 InteriorIterator::InteriorIterator(const Geometry* geom) : Iterator(geom) {
   First();
 }
-
+//------------------------------------------------------------------------------
 /// Sets the iterator to the first element
 void InteriorIterator::First() {
   _value = 1 + _increm_y;
   _valid = true;
 }
-
+//------------------------------------------------------------------------------
 /// Goes to the next element of the iterator, disables it if position is end
 void InteriorIterator::Next() {
+  if (!_valid) {return;}
   if ((_value + 1) >= _num_cell - 1 - _increm_y)
     _valid = false;
   else if ((_value + 2) % _increm_y == 0)
@@ -118,7 +119,6 @@ void InteriorIterator::Next() {
   else
     _value++;
 }
-
 //------------------------------------------------------------------------------
 /* Iterator for domain boundary cells
 */
@@ -128,7 +128,7 @@ BoundaryIterator::BoundaryIterator(const Geometry* geom) : Iterator(geom) {
   _boundary = 0;
   First();
 }
-
+//------------------------------------------------------------------------------
 /// Sets the boundary to iterate
 //  @param boundary sets which boundary to iterate
 //  0 - Bottom boundary
@@ -139,7 +139,7 @@ void BoundaryIterator::SetBoundary(const index_t& boundary) {
   _boundary = boundary;
   First();
 }
-
+//------------------------------------------------------------------------------
 /// Sets the iterator to the first element
 void BoundaryIterator::First() {
   _valid = true;
@@ -157,9 +157,10 @@ void BoundaryIterator::First() {
   else if (_boundary == 3)
     _value = _increm_y*(_increm_x - 2);
 }
-
+//------------------------------------------------------------------------------
 /// Goes to the next element of the iterator, disables it if position is end
 void BoundaryIterator::Next() {
+  if (!_valid) {return;}
   // Bottom boundary
   if (_boundary == 0) {
     if (_value + 1 >= _increm_y) {
